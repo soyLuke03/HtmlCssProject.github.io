e7341f9f93353a793b69ad1c78aa9e6555bf4c34
soyLuke03 <luquelectronico13@gmail.com>
2022-12-02 10:54:39 +0100

    cositas

--- JavaScript/reservation.js
+++ JavaScript/reservation.js
@@ -2,6 +2,20 @@ let botonReturn = document.getElementById("return");
 
 botonReturn.addEventListener('click', (e) => {
 e.preventDefault()
-    location.replace("../index.html")
+   // location.replace("../index.html")
+   
 })
+
+let span = document.getElementById("spanSubmit");
+
+window.addEventListener('submit', (e) => {
+    e.preventDefault()
+    span.classList.remove("enviado-true")
+
+    setTimeout(() => {
+        span.classList.add("enviado-true")
+    },5000)
+   
+})
+
 
